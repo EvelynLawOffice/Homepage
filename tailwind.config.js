b5a9e13ac65ccc6a1bfa9dea486b02cf63c99d91
mjs34 <mikeshef@gmail.com>
2025-03-31 22:20:12 -0400

    Normalize line endings with .gitattributes

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+      emerald: colors.emerald,
+      indigo: colors.indigo,
+      yellow: colors.yellow,
+    },
+
+    container: {
+      center: true,
+    },
+
+    extend: {},
+
+  },
+
+  plugins: [],
+}
+
